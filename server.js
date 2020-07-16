00f5da8a8c70f15f0c0a1285a157986a1f6d2300
Sadia Batool <sadia110@my.yorku.ca>
2020-07-16 16:44:41 -0400

    save notes

--- server.js
+++ server.js
@@ -11,14 +11,25 @@ app.use(express.urlencoded({extended:true}))
 
 let noteList = [ 
     {title:"first note placeholder text", text:"first note text", id:1000}
-]
-// API routes 
+] //  to save notes to db
+function saveNotes(){ 
+    false.writeFileSync( fileName, Json.stringify( noteList))
+
+} 
+
+function loadNotes(){ 
+    const loadNotes = fs.readFileSync (fileName, )
+
+}
+
+// API routes  
+
 app.get( '/api/notes', function( req, res ){ 
     console.log( `[GET/api/notes]`)  
     res.send(noteList )
 
 }) 
-
+// to post a note 
 app.post( '/api/notes/', function( req, res ){  
     let newNote= req.body  
     // to give uniqe id 
@@ -28,12 +39,16 @@ app.post( '/api/notes/', function( req, res ){
     noteList.push (newNote)
 
     res.send( newNote )
-})
+}) 
+
+// to delete a note
 
 app.delete( '/api/notes/:id', function( req, res ){ 
     const noteId = req.params.id
+// to delete a note with filter * 
+    noteList=noteList.filter( note=>note.id != noteId) 
 
-    noteList=noteList.filter( note=>note.id != noteId)
+
     console.log( `[DELETE/api/notes]`, noteList) 
     res.send({id:noteId, message: "Note deleted", status: true})
 })
